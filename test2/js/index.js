67d621c87172d438ca674b79a49aa6ce72fb43c1
Dmitriy Kachurin <kachurin.d@fmedia.tech>
2023-07-05 12:19:28 +0300

    add new landing on test

--- test2/js/index.js
+++ test2/js/index.js
@@ -76,14 +76,7 @@
     }
 
 
-    var e = (function () {
-        $(document).on("click", 'a[href^="#"]', function (i) {
-            i.preventDefault();
-            $("html, body").animate({
-                scrollTop: $($.attr(this, "href")).offset().top
-            }, 500)
-        })
-    });
+
 
     var g = (function () {
         $(".illness-box__title").click(function () {
@@ -206,38 +199,13 @@
     });
     var b = (function () {
         $(window).on("scroll resize", function () {
-            j(".x_order_form");
-            i()
+
+            // i()
         });
 
-        function j() {
-            var q = $(".banner"),
-                m = $(".main-banner"),
-                l = $(window).scrollTop(),
-                s = l + $(window).height(),
-                p = $(window).width();
-            var u = 0;
-            for (var k = 0; k < arguments.length; k++) {
-                $(arguments[k]).each(function (w, r) {
-                    var o = $(r).offset().top,
-                        x = o + $(r).outerHeight(),
-                        v = $(r).offset().left,
-                        n = v + $(r).width();
-                    o <= s && x >= l && v <= p && n >= 0 && (u += 1)
-                })
-            }
-            l <= 100 || u > 0 ? q.fadeOut() : q.fadeIn().css({
-                bottom: m.outerHeight()
-            })
-        }
 
-        function i() {
-            const k = document.querySelector(".main-banner"),
-                l = document.querySelector(".banner");
-            let redBannerHeight;
-            k ? redBannerHeight = k.clientHeight : k;
-            redBannerHeight ? l.style.bottom = redBannerHeight - 1 + "px" : l.style.bottom = 0 + "px"
-        }
+
+
     });
     $(".info__link").on("click", function () {
         var j = $(this);
@@ -314,7 +282,7 @@
     }
 
     function a() {
-        e();
+        // e();
         g();
         d();
         c();
